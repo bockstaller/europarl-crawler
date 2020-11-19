391f234c2ea95703ed9b82d9a5bda3f82d186f1f
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-11-19 23:55:30 +0100

    adds a draft of the "europarl init"-behaviour

--- src/europarl/main.py
+++ src/europarl/main.py
@@ -1,7 +1,11 @@
 import os
+import random
+import time
 
 import click
 from dotenv import find_dotenv, load_dotenv
+
+indent = "    "
 
 
 @click.group()
@@ -20,7 +24,44 @@ def cli():
     help="dry run operations and output their actions",
 )
 def init(dry_run):
-    pass
+    setup_postgres()
+    setup_redis()
 
 
 cli.add_command(init)
+
+
+def setup_postgres():
+    click.secho("Postgres:", bold=True, underline=True)
+    check_pg_connection()
+    check_table_structure()
+    create_table_structure()
+
+
+def check_pg_connection():
+    click.echo(indent + "Connection: ", nl=False)
+    time.sleep(random.random())
+    click.echo("✅")
+
+
+def check_table_structure():
+    click.echo(indent + "Table structure available: ", nl=False)
+    time.sleep(random.random())
+    click.echo("❌")
+
+
+def create_table_structure():
+    click.echo(indent + "Table structure created: ", nl=False)
+    time.sleep(random.random())
+    click.echo("✅")
+
+
+def setup_redis():
+    click.secho("Redis:", bold=True, underline=True)
+    check_redis_connection()
+
+
+def check_redis_connection():
+    click.echo(indent + "Connection: ", nl=False)
+    time.sleep(random.random())
+    click.echo("✅")
