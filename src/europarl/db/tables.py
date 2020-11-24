c20cccbe6b605a9d6ec4fb8c8449e71fcc9d8eaf
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-11-24 23:10:04 +0100

    adds preliminary urls table definition

--- src/europarl/db/tables.py
+++ src/europarl/db/tables.py
@@ -46,14 +46,60 @@ class Table:
 class SessionDay(Table):
     """
     Models the days the parliament met
+
+    Contains:
+        date: the date of a possible session
+        hit: did HEAD-ing a generated result not return a 404
+        urls_created: True if a url generator has already derived urls from the date
+        urls_created_ts: When did a url generator derive urls from the date
+
     """
 
     schema = "public"
     table_name = "session_days"
     table_definition = """CREATE TABLE IF NOT EXISTS {schema}.{table}(
+                            id integer NOT NULL,
                             date date NOT NULL,
                             hit boolean NOT NULL,
-                            checked boolean NOT NULL,
-                            checked_at time without time zone,
-                            PRIMARY KEY(date)
+                            urls_created boolean NOT NULL,
+                            urls_created_ts datetime,
+                            PRIMARY KEY(id)
+                          );"""
+
+
+class URLs(Table):
+    """
+    Stores information about URLs
+
+    Contains:
+        rule: name of the rule used to generate the URL
+        url: the generated URL
+        found: True if a crawler found this URL in a document
+        enqueued: True if the URLGenerator has enqueued the URL to be crawled, is set to False on startup
+        crawled: has this URL been crawled
+        crawled_at: when has this URL been crawled
+        file_path: where is the downloaded document stored
+        recrawl: should this file been recrawled
+        recrawl_after: the document will be recrawled sometime after this timestamp
+
+    """
+
+    schema = "public"
+    table_name = "urls"
+    table_definition = """CREATE TABLE IF NOT EXISTS {schema}.{table}(
+                            id integer NOT NULL,
+                            rule VARCHAR() NOT NULL,
+                            url TEXT NOT NULL
+                            found boolean NOT NULL,
+                            created datetime NOT NULL,
+                            enqueued boolean NOT NULL,
+                            enqueued_at datetime,
+                            enqueued_times integer NOT NULL,
+                            crawled boolean NOT NULL,
+                            crawled_at datetime,
+                            failed boolean NOT NULL,
+                            file_path TEXT,
+                            recrawl boolean NOT NULL,
+                            recrawl_after datetime,
+                            PRIMARY KEY(id)
                           );"""
