783fa89331997ebff6c22bbfb0574a2320393944
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-11-25 12:41:12 +0100

    moves documentation into the code

--- docs/code/database.rst
+++ docs/code/database.rst
@@ -8,18 +8,12 @@ Database tables
 SessionDay
 """"""""""
 
-The SessionDay table is used to store all possible days a plenary session could have occured. We check for a session by crawling for the existence of a matching protocol and store the hit (or lack of) in the hit-column.
-A job deriving all possible URLs from the date can then sort by the hit-property and create them. The creation status is tracked in urls_created and urls_created_date.
-
 .. autoclass:: europarl.db.tables.SessionDay
     :members:
 
 
 URLs
 """"
-
-The URLs table stores all crawling metadata to crawled, scheduled to be crawled  and found urls.
-This table is mainly populated by url generators which create urls to crawl by for example basing them on parliament session dates. Crawlers are then responsible for storing the crawling metadata for each url and can additionally append found urls.
 
 .. autoclass:: europarl.db.tables.URLs
     :members:
--- src/europarl/db/tables.py
+++ src/europarl/db/tables.py
@@ -70,6 +70,9 @@ class SessionDay(Table):
     """
     Models the days the parliament met
 
+    The SessionDay table is used to store all possible days a plenary session could have occured. We check for a session by crawling for the existence of a matching protocol and store the hit (or lack of) in the hit-column.
+    A job deriving all possible URLs from the date can then sort by the hit-property and create them. The creation status is tracked in urls_created and urls_created_date.
+
     Attributes:
         date : date
             the date of a possible session
@@ -98,6 +101,8 @@ class URLs(Table):
     """
     Stores information about URLs
 
+    The URLs table stores all crawling metadata to crawled, scheduled to be crawled  and found urls.
+    This table is mainly populated by url generators which create urls to crawl by for example basing them on parliament session dates. Crawlers are then responsible for storing the crawling metadata for each url and can additionally append found urls.
 
     Attributes:
 
