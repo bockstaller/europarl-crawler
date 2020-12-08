24c182eacccd2aff8eb3d504244be3e7bb22f7da
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-12-08 16:24:47 +0100

    adds table doesn't exist test

--- src/europarl/tests/db/test_sessionDay.py
+++ src/europarl/tests/db/test_sessionDay.py
@@ -1,7 +1,20 @@
 import pytest
 from europarl.db.tables import SessionDay
+from psycopg2 import sql
 
 
-def test_db(db_interface):
+def test_table_exists(db_interface):
     sessionDay = SessionDay(db_interface)
     assert sessionDay.table_exists()
+
+
+def test_table_not_exists(db_interface):
+    with db_interface.cursor() as db:
+        db.cur.execute(
+            sql.SQL("drop table {table} cascade").format(
+                table=sql.Identifier(SessionDay.table_name)
+            )
+        )
+
+    sessionDay = SessionDay(db_interface)
+    assert sessionDay.table_exists() is False
