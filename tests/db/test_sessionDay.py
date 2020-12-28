dd79d99277202d1e3ade4b2bd7577a689e7ca00a
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-12-28 16:24:51 +0100

    ports session day to new db layout

--- tests/db/test_sessionDay.py
+++ tests/db/test_sessionDay.py
@@ -75,68 +75,43 @@ def test_SessionDays_get_unchecked_days(
 
 
 @pytest.mark.parametrize(
-    "date, hit, status_code, checked, generated_url, final_url",
+    "date, hit",
     [
         (
             datetime.date.today() - datetime.timedelta(days=0),
             False,
-            404,
-            False,
-            "www.test.de",
-            "www.test1.de",
         ),
         (
             datetime.date.today() - datetime.timedelta(days=1),
             False,
-            404,
-            True,
-            "www.test.de",
-            "www.test1.de",
         ),
         (
             datetime.date.today() - datetime.timedelta(days=2),
             True,
-            200,
-            False,
-            "www.test.de",
-            "www.test1.de",
         ),
         (
             datetime.date.today() - datetime.timedelta(days=3),
             True,
-            200,
-            True,
-            "www.test.de",
-            "www.test1.de",
         ),
         (
             datetime.date.today() - datetime.timedelta(days=4),
             False,
-            404,
-            False,
-            "www.test.de",
-            "www.test1.de",
         ),
     ],
 )
-def test_SessionDays_update_day_insert(
-    db_interface, date, hit, status_code, checked, generated_url, final_url
+def test_SessionDays_insert_day(
+    db_interface,
+    date,
+    hit,
 ):
     sessionDay = SessionDay(db_interface)
-    id = sessionDay.update_day(
-        date=date,
-        status_code=status_code,
-        hit=hit,
-        checked=checked,
-        generated_url=generated_url,
-        final_url=final_url,
-    )
+    id = sessionDay.insert_day(date=date, hit=hit)
 
     with db_interface.cursor() as db:
         db.cur.execute(
-            sql.SQL(
-                "SELECT dates, hit, status_code, checked, checked_at,  generated_url, final_url FROM {table} WHERE id = %s"
-            ).format(table=sql.Identifier(SessionDay.table_name)),
+            sql.SQL("SELECT dates, hit FROM {table} WHERE id = %s").format(
+                table=sql.Identifier(SessionDay.table_name)
+            ),
             [
                 id,
             ],
@@ -145,59 +120,3 @@ def test_SessionDays_update_day_insert(
         assert len(entries) == 1
         assert date == entries[0][0]
         assert hit == entries[0][1]
-        assert status_code == entries[0][2]
-        assert checked == entries[0][3]
-        assert entries[0][4] is not None
-        assert entries[0][5] == generated_url
-        assert entries[0][6] == final_url
-
-
-def test_SessionDays_update_day_upsert(db_interface):
-    sessionDay = SessionDay(db_interface)
-    date = datetime.date.today()
-    id = sessionDay.update_day(
-        date=date,
-        status_code=200,
-        hit=False,
-        checked=False,
-        generated_url="www.test.de",
-        final_url="www.test1.de",
-    )
-
-    ts = None
-
-    with db_interface.cursor() as db:
-        db.cur.execute(
-            sql.SQL("SELECT dates, checked_at FROM {table} WHERE id = %s").format(
-                table=sql.Identifier(SessionDay.table_name)
-            ),
-            [
-                id,
-            ],
-        )
-        entries = db.cur.fetchall()
-        assert len(entries) == 1
-        assert date == entries[0][0]
-        ts = entries[0][1]
-
-    id = sessionDay.update_day(
-        date=date,
-        status_code=200,
-        hit=False,
-        checked=False,
-        generated_url="www.test.de",
-        final_url="www.test1.de",
-    )
-    with db_interface.cursor() as db:
-        db.cur.execute(
-            sql.SQL("SELECT dates, checked_at FROM {table} WHERE id = %s").format(
-                table=sql.Identifier(SessionDay.table_name)
-            ),
-            [
-                id,
-            ],
-        )
-        entries = db.cur.fetchall()
-        assert len(entries) == 1
-        assert date == entries[0][0]
-        assert ts != entries[0][1]
