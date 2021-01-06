be74ebebb85ee4456ec9711a8c88fd228f0e09dd
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-01-06 16:43:30 +0100

    adds methods for postprocessing the documents

--- europarl/db/documents.py
+++ europarl/db/documents.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime, timezone
 
 from .tables import Table
@@ -12,6 +13,8 @@ class Documents(Table):
                             id SERIAL,
                             filepath VARCHAR(2000),
                             filename uuid UNIQUE,
+                            enqueued boolean DEFAULT False,
+                            data jsonb,
                             downloaded_at timestamp with time zone,
                             CONSTRAINT documents_pkey PRIMARY KEY (id)
                           );"""
@@ -37,3 +40,80 @@ class Documents(Table):
             result = db.cur.fetchone()[0]
 
         return result
+
+    def get_unprocessed_documents(self, limit=10):
+        query = """ SELECT rules.id, rules.rulename, documents.id, documents.filepath
+                    FROM documents
+                    LEFT JOIN requests ON requests.document_id=documents.id
+                    LEFT JOIN urls on requests.url_id=urls.id
+                    LEFT JOIN rules on urls.rule_id=rules.id
+                    WHERE documents.data is NULL AND rules.rulename is not NULL
+                    ORDER by requests.requested_at ASC
+                    LIMIT %s
+                """
+        with self.db.cursor() as db:
+            db.cur.execute(
+                query,
+                [limit],
+            )
+            documents = db.cur.fetchall()
+
+        result = []
+        for item in documents:
+            result.append(
+                {
+                    "rule": {"id": item[0], "name": item[1]},
+                    "document": {"id": item[2], "path": item[3]},
+                }
+            )
+
+        return result
+
+    def mark_as_enqueued(self, document_id):
+        query = """ UPDATE documents
+                    SET enqueued = True
+                    WHERE documents.id = %s
+                """
+
+        with self.db.cursor() as db:
+            db.cur.execute(
+                query,
+                [document_id],
+            )
+
+    def reset_enqueued(self):
+        query = """ UPDATE documents
+                    SET enqueued = False
+                    WHERE documents.data is Null
+                """
+
+        with self.db.cursor() as db:
+            db.cur.execute(
+                query,
+            )
+
+    def get_data(self, document_id):
+        query = """ SELECT documents.id, documents.data
+                    FROM documents
+                    WHERE documents.id = %s"""
+
+        with self.db.cursor() as db:
+            db.cur.execute(
+                query,
+                [document_id],
+            )
+            document = db.cur.fetchone()
+
+        return {"id": document[0], "data": json.loads(document[1])}
+
+    def set_data(self, document_id, data):
+        query = """ UPDATE documents
+                    SET data = %s
+                    WHERE documents.id=%s
+                """
+
+        with self.db.cursor() as db:
+            db.cur.execute(
+                query,
+                [json.dumps(data), document_id],
+            )
