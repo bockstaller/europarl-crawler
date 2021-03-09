a0f539423af6c7bdf2eaef9f67733dc53fe152a3
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-03-09 14:29:31 +0100

    adds entrypoints

--- setup.py
+++ setup.py
@@ -3,5 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name="europarl",
     version="0.1",
-    packages=["europarl"],
+    zip_safe=False,
+    py_modules=["eurocli"],
+    install_requires=[
+        "Click",
+    ],
+    entry_points="""
+        [console_scripts]
+        eurocli=europarl.eurocli:main
+    """,
 )
